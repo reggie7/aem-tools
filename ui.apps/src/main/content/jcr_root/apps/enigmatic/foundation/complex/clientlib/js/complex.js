ee8e84a526ccf34af69a8b9964e770dd0cd2f3cd
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2017-09-13 13:20:35 +0200

    Base zoom component with author clientlibs that will allow a switch between normal and zoom mode.

---
+++ ui.apps/src/main/content/jcr_root/apps/enigmatic/foundation/complex/clientlib/js/complex.js
@@ -0,0 +1,53 @@
+window.enigmatic = window.enigmatic || {};
+
+window.enigmatic.author = window.enigmatic.author || {};
+
+window.enigmatic.author.Complex = function(sourceURL, targetResource, targetMode) {
+
+	this.source = new (function(url) {
+		this.url = url;
+
+		// let's find the current mode
+		var modes = CQ.shared.HTTP.getSelectors(url);
+		if (modes != null) {
+			this.mode = modes[0];
+		}
+		this.hasMode = function() { return this.mode !== undefined; };
+	})(sourceURL);
+
+	this.target = new (function(resource, mode) {
+		this.resource = resource;
+		this.mode = mode;
+		// since the resource to be presented in the special mode within its page - we can find the page path from the resource path
+		this.page = resource.substring(0, resource.indexOf("/jcr:content/"));
+	})(targetResource, targetMode);
+
+	this.reload = new (function(source, target) {
+		// the clean page url without any selectors and other additions
+		this.page = source.url.substring(0, source.url.indexOf(target.page)) + target.page + ".html";
+
+		this.getURL = function() {
+			var url = this.page;
+			if (this.mode !== undefined) {
+				url = CQ.shared.HTTP.addSelector(url, this.mode, 0);
+			}
+			return url;
+		};
+	})(this.source, this.target);
+
+	if (!this.source.hasMode()) {
+		this.reload.mode = this.target.mode;
+		this.reload.resource = this.target.resource;
+	}
+};
+
+window.enigmatic.author.Complex.toggle = function(comp, targetMode) {
+	var sourceURL = window.document.URL;
+	var targetResource = comp.path;
+	var complex = new window.enigmatic.author.Complex(sourceURL, targetResource, targetMode);
+	CQ.shared.Util.reload(window, complex.reload.getURL());
+};
+
+window.enigmatic.author.Complex.zoom = function(comp) {
+	window.enigmatic.author.Complex.toggle(comp, "zoom");
+};
