46b514018478e1ebea78a6fe19a2345035ea08f7
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2017-09-18 11:52:53 +0200

    collapse code backup - it does not work yet it might never work unfortunately since e.g. http://localhost:4502/editor.html/content/enigmatic/expandable/jcr:content/content/expandable.html shows empty page - the editor might work only for pages, not simple non-page resources, we'll check it later next year :)

--- ui.apps/src/main/content/jcr_root/apps/enigmatic/foundation/complex/clientlib/js/expand.js
+++ ui.apps/src/main/content/jcr_root/apps/enigmatic/foundation/complex/clientlib/js/expand.js
@@ -5,3 +5,7 @@ window.enigmatic.author = window.enigmatic.author || {};
 window.enigmatic.author.expand = function(comp) {
 	CQ.shared.Util.reload(window, comp.path + ".expand.html");
 };
+
+window.enigmatic.author.collapse = function(comp) {
+	CQ.shared.Util.reload(window, "/editor.html" + comp.path + ".html");
+};
