9ba6e8e34b454596f263c38270795a848c1b6827
Radosław Wesołowski <radoslaw.wesolowski@hi-res.de>
2017-10-28 18:09:39 +0200

    Better way to resolve the Cloud Config using the hierarchy and inheritance

--- core/src/main/java/pl/enigmatic/aem/config/impl/CloudServiceResolverImpl.java
+++ core/src/main/java/pl/enigmatic/aem/config/impl/CloudServiceResolverImpl.java
@@ -1,11 +1,12 @@
 package pl.enigmatic.aem.config.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.resource.ResourceResolver;
 
-import com.day.cq.commons.inherit.HierarchyNodeInheritanceValueMap;
-import com.day.cq.commons.inherit.InheritanceValueMap;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.webservicesupport.Configuration;
 import com.day.cq.wcm.webservicesupport.ConfigurationConstants;
@@ -17,14 +18,20 @@ import pl.enigmatic.aem.config.CloudServiceResolver;
 @Component
 public class CloudServiceResolverImpl implements CloudServiceResolver {
 
+	private static final String[] EMPTY_PATHS = new String[]{};
+
 	@Override
 	public Configuration resolve(final Page page, final String name) {
 		final ResourceResolver resourceResolver = page.getContentResource().getResourceResolver();
 		final ConfigurationManager manager = resourceResolver.adaptTo(ConfigurationManager.class);
 		if (manager != null) {
-			final InheritanceValueMap pageProperties = new HierarchyNodeInheritanceValueMap(page.getContentResource());
-			final String[] paths = pageProperties.getInherited(ConfigurationConstants.PN_CONFIGURATIONS, new String[]{});
-			return manager.getConfiguration(name, paths);
+			final ArrayList<String> paths = new ArrayList<>();
+			Page p = page;
+			while (p != null) {
+				Collections.addAll(paths, p.getProperties().get(ConfigurationConstants.PN_CONFIGURATIONS, EMPTY_PATHS));
+				p = p.getParent();
+			}
+			return manager.getConfiguration(name, paths.toArray(new String[paths.size()]));
 		}
 		return null;
 	}
