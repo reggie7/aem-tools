22d2606dab8e8c23270c5457142c1f048bead2da
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2025-06-03 10:51:34 +0200

    Adjust deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-source ~/.bash_profile
-set -e
-MAVEN_LOCATION=${1:-$(which mvn)}
-bash "${MAVEN_LOCATION}" clean install -P autoInstallPackage -P autoInstallContent
-bash "${MAVEN_LOCATION}" clean install -P autoInstallPackagePublish -P autoInstallContentPublish
+if [ -d "$HOME/.sdkman" ]; then
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+    sdk version
+
+    all_installed=true
+    while IFS='=' read -r candidate version; do
+        [[ "$candidate" =~ ^#.*$ || -z "$candidate" ]] && continue
+        candidate=$(echo "$candidate" | xargs)
+        version=$(echo "$version" | xargs)
+        if ! sdk list "$candidate" | grep -qE " $version(\s|\*|\+|$)"; then
+            echo "$candidate $version is NOT installed."
+            all_installed=false
+        else
+            echo "$candidate $version is installed."
+        fi
+    done < .sdkmanrc
+
+    if $all_installed; then
+        sdk env
+    else
+      sdk env install
+    fi
+fi
+
+./mvnw clean install -P autoInstallPackage -P autoInstallContent "$@"
+./mvnw clean install -P autoInstallPackagePublish -P autoInstallContentPublish "$@"
